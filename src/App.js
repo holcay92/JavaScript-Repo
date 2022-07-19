feb4247770f2346a3b4d92add532f75776cbd5f3
Halil Olcay <holcay92@gmail.com>
2022-07-19 15:19:06 +0300

    spin rate and some background colors are changed to observe

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>{"I am spinning!!!"}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-{"Welcome to Setur :)"}        </p>
+          {"Welcome to Setur :)"}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click To Learn React With Setur
         </a>
       </header>
     </div>
