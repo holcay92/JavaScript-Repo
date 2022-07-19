9fc2d1b0535602fbecb1bb08d11044a1dea4e247
Halil Olcay <holcay92@gmail.com>
2022-07-19 16:14:12 +0300

    counter increase decrease feature worked

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App =() => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
+      
       <header className="App-header">
+        <button  onClick={() => setCounter((prevCount) => prevCount - 1)}> -</button>
+        <h1> {counter}</h1>
+        <button onClick={() => setCounter((prevCount) => prevCount + 1)}> +</button>
+
         <p>{"I am spinning!!!"}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {"Welcome to Setur :)"}
         </p>
+        <h3> Hello Halil</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
